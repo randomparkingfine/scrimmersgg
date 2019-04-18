2438b6d6ae391e1325c99c1031ffcda9098da2e8
shockrahwow <alejandros714@protonmail.com>
2019-04-17 18:51:56 -0700

    + Added some base level links + Proper game linking now

--- index.php
+++ index.php
@@ -4,11 +4,22 @@ require 'AltoRouter.php';
 $router = new AltoRouter();
 
 // Base level pages
-$router->map('GET', '/[a:basePage]', function ($baseGame) {
-	// get that games page
-	require __DIR__ . '/static/html/' . $baseGame . '.html';
+$router->map('GET', '/', function() {
+	require __DIR__ . '/static/html/index.html';
 });
 
+$router->map('GET', '/about', function() {
+	require __DIR__ . '/static/html/about.html';
+});
+
+$router->map('GET', '/signup', function() {
+	require __DIR__ . '/static/html/signup.html';
+});
+
+// games 
+$router->map('GET', '/game/[a:game]', function($game) {
+	require __DIR__ . '/static/html/' . $game . '.html';
+});
 $match=$router->match();
 
 if(is_array($match) && is_callable($match['target'])){
