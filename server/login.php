13fbb5b866bade7198f72494b6233d2c908c3ac8
Medium Fries <alejandros714@protonmail.com>
2019-05-04 14:57:24 -0700

    quick backend chekc for login

---
+++ server/login.php
@@ -0,0 +1,15 @@
+<?php
+require __DIR__ . 'db.php';
+require __DIR__ . '/../vendor/autoload.php';
+use Medoo\Medoo;
+
+$db = new Medoo($cleardb_config);
+$users = $db->select('users', ['username', 'password']);
+foreach($users as $item) {
+	if($_POST['username'] == $item['username']) {
+		if($_POST['password']) {
+			echo 'good';
+		}
+	}
+}
+?>
