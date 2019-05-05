b0c9a6af20082543214717319e0c09b75713530b
Maxime Lizandier <mlizandier@csumb.edu>
2019-05-05 14:56:15 -0700

    [Calendar] changed some problems I guess

--- server/db.php
+++ server/db.php
@@ -1,10 +1,10 @@
 <?php
 $cleardb_config = array(
 		'database_type' => 'mysql',
-		'database_name' => "heroku_4f58a1b681d6fa5",//getenv('CLEARDB_NAME'),
-		'server' => "us-cdbr-iron-east-02.cleardb.net",//getenv('CLEARDB_HOST'),
-		'username' => "16f9125243321f4",//getenv('CLEARDB_USERNAME'),
-		'password' => "b076f7bfe24b18"//getenv('CLEARDB_PASSWORD')
+		'database_name' => getenv('CLEARDB_NAME'),
+		'server' => getenv('CLEARDB_HOST'),
+		'username' => getenv('CLEARDB_USERNAME'),
+		'password' => getenv('CLEARDB_PASSWORD')
 );
 
 ?>
---
+++ server/getSchedule.php
@@ -0,0 +1,19 @@
+<?php
+    require __DIR__ . '/../vendor/autoload.php';
+    require __DIR__ . '/db.php';
+    use Medoo\Medoo;
+    session_start();
+    $db = new Medoo($cleardb_config);
+    //$email = $_SESSION['email'];   
+     $email = 'zelmhirst19@army.mil';
+    //$email = 'rmacfarlanea@opensource.org'; 
+    //$email = 'ppallentv@ask.com';
+    $datas=$db->select('users',[
+        'user_schedule'
+    ],
+    [
+        'email'=>$email
+    ]);
+
+    echo json_encode($datas);   
+?>
