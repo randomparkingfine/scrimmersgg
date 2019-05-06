58ab778f74d816a7dbd8bf07bb4ad3a206632965
shockrahwow <alejandros714@protonmail.com>
2019-05-06 00:27:05 -0700

    setup html frame for user information

--- pages/html/userPage.php
+++ pages/html/userPage.php
@@ -1,3 +1,9 @@
+<?php
+// Getting the relevant user info from the uri
+require __DIR__ . '/../../vendor/autoload.php';
+//require __DIR__ . '/../../userMail.php';	// this does nothing yet but will in the coming days
+use Medoo\Medoo;
+?>
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -18,7 +24,7 @@
 			<nav id = "nav">
 				<ul class="links">
 					<li><a href="/">Home</a></li>
-					<li class="active"><a href="/about">About</a></li>
+					<li><a href="/about">About</a></li>
 					<?php
 					require __DIR__ . '/../../server/navbar.php';
 					if(!empty($_SESSION)) {
@@ -41,29 +47,19 @@
 				<section class = "post">
 					<div class = "row">
 						<div class ="col-6 col-12-small">
-							<h2>User Bio</h2>
-							<p>Felis sagittis eget tempus primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget. tempus euismod. Magna sed etiam ante ipsum primis in faucibus vestibulum.</p>
-							<h2>User Links</h2>
-							<ul class="icons">
-								<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-								<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-								<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-								<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
-								<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
-							</ul>
+							<h2>Bio</h2>
+							<h2>Links</h2>
 						</div>
 						<div class ="col-6 col-12-small">
-							<h2>User Games</h2>
-							<ul>
-								<li>Dolor pulvinar etiam.</li>
-								<li>Sagittis lorem eleifend.</li>
-								<li>Felis feugiat dolore viverra.</li>
-								<li>Dolor pulvinar etiam.</li>
-							</ul>
-							<h2>User Team</h2>
-							<p>Felis sagittis eget tempus primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget. tempus euismod. Magna sed etiam ante ipsum primis in faucibus vestibulum.</p>
+							<h2>Games</h2>
+							<h2>Team</h2>
 						</div>
 					</div>
+				</section>
+				<section id="user-schedule" class="post">
+					<header>
+						<h2>Available Times</h2>
+					</header>
 				</section>
 			</div>
 			
@@ -74,14 +70,7 @@
 				<form method="post" action="#">
 					<div class="fields">
 						<div class="field">
-							<label for="name">Name</label>
-							<input type="text" name="name" id="name" />
-						</div>
-						<div class="field">
-							<label for="email">Email</label>
-							<input type="text" name="email" id="email" />
-						</div>
-						<div class="field">
+							<h2 id="response">Send you're message here</h2>
 							<label for="message">Message</label>
 							<textarea name="message" id="message" rows="3"></textarea>
 						</div>
@@ -91,38 +80,7 @@
 					</ul>
 				</form>
 			</section>
-			<section class="split contact">
-				<section class="alt">
-					<h3>Address</h3>
-					<p>1234 Somewhere Road #87257<br />
-					Nashville, TN 00000-0000</p>
-				</section>
-				<section>
-					<h3>Phone</h3>
-					<p><a href="#">(000) 000-0000</a></p>
-				</section>
-				<section>
-					<h3>Email</h3>
-					<p><a href="#">info@untitled.tld</a></p>
-				</section>
-				<section>
-					<h3>Social</h3>
-					<ul class="icons alt">
-						<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
-						<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
-					</ul>
-				</section>
-			</section>
 		</footer>
-
-	<!-- Copyright -->
-		<div id="copyright">
-			<ul><li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
-		</div>
-
-</div>
 		<!-- Scripts -->
 		<script src="assets/js/jquery.min.js"></script>
 		<script src="assets/js/jquery.scrollex.min.js"></script>
