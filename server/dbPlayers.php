bf3f3c80ad405b1da0e659cc5857052b810a6728
Francisco <fherrera-guerrero@csumb.edu>
2019-05-07 15:27:44 -0700

    no change?

--- server/dbPlayers.php
+++ server/dbPlayers.php
@@ -2,18 +2,16 @@
     require __DIR__ . '/../vendor/autoload.php';
     require __DIR__ . '/db.php';
     use Medoo\Medoo;
-//    session_start();
     
     
     $db = new Medoo($cleardb_config);
     
     
-    //["region[=]"=>$_POST["regions"]
     $data = $db->select('users', "*",["username[~]"=>$_POST['usernames']]);
     
     echo json_encode($data);
     
     
     
-    ?>
+?>
 
