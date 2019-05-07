57d8721729338ce8512119c2298eb5e90fccec9e
Francisco <fherrera-guerrero@csumb.edu>
2019-05-07 15:26:39 -0700

    no change i hope

--- pages/js/userPage.js
+++ pages/js/userPage.js
@@ -1,11 +1,10 @@
 $("#submit").on("click",function () {
-	console.log("works");
 	$.ajax({
 		url:"/server/user.php",
 		type:"POST",
 		dataType:"json",
 		data:{
-			"msg":$("message").val(),
+			"msg":$("#message").val(),
 		},
 		success:function (data) {
 			console.log(data);
